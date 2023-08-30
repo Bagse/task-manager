0a9cb90dadb260e654acc54c56097ca3a2e4233b
bagse <102260190+Bagse@users.noreply.github.com>
2023-08-30 00:13:56 -0500

    fix axios

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://bd-crud-mongo.vercel.app/api",
   withCredentials: true,
 });
 
